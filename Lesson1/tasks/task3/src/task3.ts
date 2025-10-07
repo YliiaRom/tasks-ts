c5ebb451580a20197f33734eac82fd9dcb8b2d14
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-10-07 17:21:29 +0300

    add task3

--- Lesson1/tasks/task3/src/task3.ts
+++ Lesson1/tasks/task3/src/task3.ts
@@ -1,12 +1,66 @@
+//Вводиться назва продукту, ціна одиниці та кількість для 2-х видів товарів. Вивести чек про купівлю.
+
+let btn = document.getElementById("btnTask3");
+let message = document.getElementById("solution3");
+
 let product1 = "";
 let product2 = "";
 
-let valPrice: number;
+type ProductData = {
+  name: string;
+  price: number;
+  quantity: number;
+  sum: number;
+};
 
-let price: unknown = prompt("Введіть ціну ...");
+btn?.addEventListener("click", () => {
+  let products: ProductData[] = [];
 
-if (typeof price === "string") {
-  valPrice = parseInt(price);
-} else {
-  alert("Вводити можно лише числа! ");
-}
+  for (let i = 1; i <= 2; i++) {
+    const dataProduct = prompt(`Введіть назва продукту №${i}:`);
+    const dataPrice = prompt(`Ціна за Товар№${i}:`);
+    const dataQuantity = prompt(`Кількість Товару№${i}:`);
+    let priceValue = parseFloat(dataPrice || "0");
+    let priceQuantity = parseFloat(dataQuantity || "0");
+
+    //- not valid
+    if (
+      !dataProduct ||
+      isNaN(priceValue) ||
+      priceValue < 0 ||
+      isNaN(priceQuantity) ||
+      priceQuantity < 0
+    ) {
+      alert("Ви вводили не коректні значення, або не ввели якісь значення");
+      return;
+    }
+
+    //-valid
+    products.push({
+      name: dataProduct,
+      price: priceValue,
+      quantity: priceQuantity,
+      sum: priceValue * priceQuantity,
+    });
+  }
+
+  //-check
+
+  let receipt = `<h4>Чек про купівлю:</h4>`;
+  let totalSum = 0;
+
+  products.forEach((product, index) => {
+    receipt += `<p>назва Товару${index + 1}: ${
+      product.name
+    } </p> <p>ціна одиниці:${product.price}</p> 
+    <p>кількість: ${product.quantity};</p>
+    <p class="sum">Загальна сумма за товари ${product.name}: ${
+      product.sum
+    };</p> `;
+
+    totalSum += product.sum;
+  });
+  receipt += `<h2> Сумма: ${totalSum}грн</h2>`;
+  //-message
+  message!.innerHTML = receipt;
+});
